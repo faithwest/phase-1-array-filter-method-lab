2a47e9a4a6e957b28d069eb2216bfb45cc159dd6
faithwest <faithchepkoech425@gmail.com>
2023-10-17 12:10:05 +0300

    assignment finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+// This function to take drivers and a name as instructions.
+// filter through and pick the given driver names., 
+// regardless if upper or lowercase).
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  //  function to take drivers and a group of letters as instructions.
+  // filter through and pick the given driver names.
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  //  function to take drivers and a name as instructions.
+  // filter through and pick the given driver names.
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
